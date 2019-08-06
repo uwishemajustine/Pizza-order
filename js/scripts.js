4386a504c1c43fadd4f41638d34ad5ef130d6650
uwishemajustine <alsenjastine90@gmail>
2019-08-06 11:23:09 +0200

    added some changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,91 +1,99 @@
 // var pizzaPrice = 0;
 // var pizza;
-function menu (size, toppings, crust) {
+function Menu (size, toppings, crust) {
     this.size = size;
     this.crust = crust;
     this.toppings = toppings;
-    this.menu = [];
+    this.number = number;
   }
-  menu.prototype.pizza = function() {
-    return this.size + " " + this.crust + " " + this.toppings;
+  Menu.prototype.fullPizza = function() {
+    return this.size + " " + this.crust + " " + this.toppings + " " +this.number;
   }
     // user interface logic
     $(document).ready(function() {
 
-      $("form#menu").submit(function(event) {
+      $("button#Order").submit(function(event) {
         event.preventDefault();
-         $("Order").click(function() {
-        var inputtedSize = $("radio#size").val();
-        console.log("inputtedSize")
-        var inputtedCrust = $("select#crust").val();
-        console.log("inputtedCrust");
-        var inputtedToppings = $("select#toppings").val();
-        console.log("inputtedToppings") 
-        var newPizza = new Menu(inputtedSize, inputtedCrust, inputtedToppings);
-    
-         });
-      });
-    });
-
-        // $("ul#pizza-order").append("<li><button type='submit' id='submit'>Order</button>" + " Pizza Order" + "</button></span></li>");
-    
+        //  $("#order").click(function() {
+          if ($("#inlineRadio1:checked").val()) {
+        var inputtedSize = $("#inlineRadio1:checked").val();
+        console.log(inputtedSize);
+        var cash1= 5
+       }else if ($("#inlineRadio2:checked").val()) {
+          var inputtedSize = $("#inlineRadio2:checked").val();
+          console.log(inputtedSize);
+          var cash1= 3
+        }else if ($("#inlineRadio3:checked").val()) {
+          var inputtedSize = $("#inlineRadio3:checked").val();
+        console.log(inputtedSize);
+        var cash1= 7
+        }else {
+          alert("Refresh")
+        }
         
-    
-// function Pizza(size) {
-//   this.pizzasize = size;
-//   this.pizzacrust = crust;
-//   this.toppings = toppings;
-//   this.pizza = [];
-// };
+        var inputtedCrust = $("select#crust").val();
+        if ($("select#crust").val==="Garlic Butter Crust") {
+          var cash2 = 8
+        }else if ($("select#crust").val==="Spicy Crust") {
+          var cash2 = 11
+        }else if ($("select#crust").val==="Plain Crust") {
+          var cash2 = 13
+        }else if ($("select#crust").val==="Thick Crust") {
+          var cash2 = 12
+        }else if ($("select#crust").val==="Flatbread Crust") {
+          var cash2 = 14
+        }else if ($("select#crust").val==="Cheese-Stuffed Crust") {
+          var cash2 = 16 
+        }else {
+          alert("Refresh and order again")
+        }
 
-// // Price Prototype Method
-// Pizza.prototype.price = function() {
-//   var price = 7;
+        var inputtedToppings = $("select#toppings").val();
+        if ($("select#toppings").val==="Onions") {
+          var cash3 = 15
+        }else if ($("select#toppings").val==="Green Peppers") {
+          var cash3 = 18
+        }else if ($("select#toppings").val==="Black Olives") {
+          var cash3 = 20
+        }else if ($("select#toppings").val==="Spinach") {
+          var cash3 = 22
+        }else if ($("select#toppings").val==="Mushrooms") {
+          var cash3 = 21
+        }else if ($("select#toppings").val==="Chicken") {
+          var cash3 = 27 
+        }else if ($("select#toppings").val==="Pepperoni") {
+          var cash3 = 28
+        }else if ($("select#toppings").val==="Beef") {
+          var cash3 = 29
+        }else if ($("select#toppings").val==="Sausage") {
+          var cash3 = 19  
+        }else {
+          alert("Refresh and order again")
+        }
+        var inputtedNumber = $("newnumber#number");
+        // console.log(inputtedNumber);
+        var newPizza = new Menu(inputtedSize, inputtedCrust, inputtedToppings, inputtedNumber);
+        var pizzaCash = parseInt(cash1) + parseInt(cash2) + parseInt(cash3);
+        var totalCash = pizzaCash + inputtedNumber;
+        var finaltotal = totalCash + 2;
+        $("ul#responses").append("<li>" +newPizza.fullPizza() + totalcash + finaltotal +"</li>");
+        // console.log(newPizza.fullPizza() )
+        // $("ul#responses").append("<li>" + "You will pay" + totalCash + $+"</li>");
+        // console.log(newPizza.pizzacash)
+        // $("#del").click(function(){
+        //   $("ul#responses").append("<li>" + "total to be paid" + total + $+"</li>");
+  //       $("#newPizza").text("");
+  // $("#newPizza").append(Pizza);
+  // var totalCash = newPizza.pizzaCash("inputtedSize", "inputtedCrust","inputtedToppings", "inputtedNumber");
+  // $("#seen").append("Your total is $" + totalCash + ".");
+  });
+});   
 
-//   if (this.pizzasize === "Small") {
-//     price += 2;
-//   } else if (this.pizzasize === "Medium") {
-//     price += 5;
-//   } else if (this.pizzasize === "Large") {
-//     price += 10;  
-//   } else {
-//     price += 1;
-//   }
-
-//   if (this.pizzasize.length === 0) {
-//     price += 1;
-//   } else {
-//     price += this.pizzacrust.length;
-//   }
-
-//   return price;
-// };
-
-// // Front-End User Logic
-// $(document).ready(function() {
-//   $("form#pizza-form").submit(function(event) {
-//     event.preventDefault();
-
-//     var inputtedPizzaSize = $("radio#pizza-size").val();
-//     var inputtedPizzaCrust = $("radio#pizza-crust").val();
-//     var inputtedPizzaTopping = $("radio#pizza-topping").val();
-//     var newPizza = new Pizza(inputtedPizzaSize, inputtedPizzaCrust, inputtedPizzaTopping);
-
-//     $.each($("input[name='toppings']:checked"), function() {
-//       newPizza.pizza.push($(this).val());
-// console.log()
-//     });
-
-//     $("ul#pizza-order-list").append("<li><button type='submit' id='submit'>Order</button>" + newPizza.pizzasize + " Pizza Order" + "</button></span></li>");
-
-//     $(".pizzaOrder").last().click(function() {
-//       $("#pizza-order-detail").show();
-//       $(".pizza-size").text(newPizza.pizzasize);
-//       $(".pizza-crust").text(newPizza.pizzacrust);
-//       $(".pizza-toppings").text(newPizza.pizzatoppings);
-//       $(".order-total").text(newPizza.price());
-
-//     });
-//   });
-// });
+    $(document).ready(function() {
+      $("#del").click(function() {
+      alert("Please tell us your location and will send you your delivery");
+      prompt("Please enter your location(house number, street, village)");
+      alert("The charge for delivery is $8 , your receive your delivery in 30 minutes thanks for shopping with jas's pizza we are willing to serve you again")
+    });
+    });
   
